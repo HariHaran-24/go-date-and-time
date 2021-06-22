4880026fbe93ada83520ff33750a54c2aec8a30f
HariHaran-24 <58470609+HariHaran-24@users.noreply.github.com>
2021-06-22 22:54:25 +0530

    Add files via upload

---
+++ app.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	clock:= time.Now()
+	fmt.Fprintf(w,"<body style=background-color:#9933ff;>",)
+	fmt.Fprintf(w,"<h1><i>Date :: %s </i></h1>\n",clock.Format("Mon 2-Jan-2006"))
+	fmt.Fprintf(w,"<h1><i>Time :: %s </i></h1>\n",clock.Format("03:04:05 PM"))
+	fmt.Fprintf(w,"</body>")
+	
+}
+
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil)) // this will run on port 6543 , if you run on local system means go to browser type localhost:6544
+
+}
